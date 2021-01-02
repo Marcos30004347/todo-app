d1b496bb58c1bee4ba07a892579458c056f8423a
Marcos <marcos30004347@gmail.com>
2021-01-01 23:14:20 -0300

    add more ssl/tls secutiry stuff

--- shared/ruby/lib/todo-grpc.rb
+++ shared/ruby/lib/todo-grpc.rb
@@ -3,6 +3,8 @@
 # https://github.com/grpc/grpc/blob/master/src/ruby/spec/channel_credentials_spec.rb
 # https://bbengfort.github.io/programmer/2017/03/03/secure-grpc.html#:~:text=gRPC%20has%20SSL%2FTLS%20integration,provide%20certificates%20for%20mutual%20authentication.
 
+require 'grpc'
+
 class GRPCServer
     # def self.inherited(subclass)
     #     @subclass = subclass
@@ -13,10 +15,22 @@ class GRPCServer
     #         handler(hello_req)
     #     end
     # end
-
+    def load_test_certs
+        test_root = File.join(File.dirname(__FILE__), 'testdata')
+        files = ['ca.pem', 'server1.key', 'server1.pem']
+        files.map { |f| File.open(File.join(test_root, f)).read }
+    end
+    
     def initialize(handler, url)
+        this_dir = File.expand_path(File.dirname(__FILE__))
+        certs_dir = File.join(this_dir, '../certs')
+
+        files = ['ca.pem', 'server.key', 'server.pem']
+        certs = files.map { |f| File.open(File.join(certs_dir, f)).read }
+
+        creds = GRPC::Core::ServerCredentials.new(certs[0], [{ private_key: certs[1], cert_chain: certs[2] }], true)
         @server = GRPC::RpcServer.new
-        @server.add_http2_port(url, :this_port_is_insecure)
+        @server.add_http2_port(url, creds)
         @server.handle(handler)
     end
 
@@ -28,7 +42,14 @@ end
 
 class GRPCClient
     def initialize(url)
-        @stub = Helloworld::Greeter::Stub.new(url, :this_channel_is_insecure)
+        this_dir = File.expand_path(File.dirname(__FILE__))
+        certs_dir = File.join(this_dir, '../certs')
+
+        files = ['ca.pem', 'client.key', 'client.pem']
+        certs = files.map { |f| File.open(File.join(certs_dir, f)).read }
+    
+        creds = GRPC::Core::ChannelCredentials.new(certs[0], certs[1], certs[2])
+        @stub = Helloworld::Greeter::Stub.new(url, creds)
     end
 
     def add_endpoint(method)
